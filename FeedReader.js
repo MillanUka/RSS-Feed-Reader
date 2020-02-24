f8108e76a5515f843a5706df37e2ea9e35e2390e
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-24 15:46:37 +1300

    Changes Styling
    Also removed the vox rss link as default as there was problem retrieving it, for some reason

--- FeedReader.js
+++ FeedReader.js
@@ -6,18 +6,21 @@ userFeedURLs.forEach(userUrl => {
         type: 'GET',
         url: API + userUrl,
         dataType: 'jsonp',
-        success: function(data) {
+        success: function (data) {
             console.log(data);
             data.items.forEach(item => {
                 var content = document.getElementById('content');
 
-                content.innerHTML += "<div class=\"itemtitle\"><a href=\"" + item.link + "\"><h1>" + item.title + "</h1></a>" + "<h4> from " + data.feed.title + "</h4></div>";
+                var newItem = "";
+                newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h1>" + item.title + "</h1></a>" + "<h4> from " + data.feed.title + "</h4>";
                 if (item.author != "")
-                    content.innerHTML += "<h4> By " + item.author + "</h4>";
+                    newItem += "<h4> By " + item.author + "</h4>";
 
-                content.innerHTML += "<h4>Published Date: " + item.pubDate + "</h4>";
+                newItem += "<h4>Published Date: " + item.pubDate + "</h4>";
 
-                content.innerHTML += "<div class=\"desc\">" + item.description + "<hr><div>";
+                newItem += item.description + "<hr></div>";
+
+                content.insertAdjacentHTML('beforeend', newItem);
             });
         }
     });
